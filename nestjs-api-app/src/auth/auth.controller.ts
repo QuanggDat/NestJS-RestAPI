cf55e0eee2910ddc6fb7d269cb2e3aceefb1be89
QuanggDat <datqit@gmail.com>
2026-08-18 16:09:15 +0700

    feat: implement authentication module with register and login endpoints

--- nestjs-api-app/src/auth/auth.controller.ts
+++ nestjs-api-app/src/auth/auth.controller.ts
@@ -1,9 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
-@Controller()
+//all routes in this controller are prefixed with /auth
+@Controller('auth')
 export class AuthController {
   //auth service is automatically created when initializing the controller
   constructor(private authService: AuthService) {}
+
+  //POST: .../auth/register
+  @Post('register') //register a new user
+  register() {
+    return this.authService.register();
+  }
+
+  //POST: .../auth/login
+  @Post('login')
+  login() {
+    return this.authService.login();
+  }
 }
 //export = "make public"
--- nestjs-api-app/src/auth/auth.service.ts
+++ nestjs-api-app/src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
-@Injectable({}) //this is "Dependency Injection" for AuthService
-export class AuthService {}
+@Injectable() //this is "Dependency Injection"
+export class AuthService {
+  register() {
+    return { message: 'register' };
+  }
+
+  login() {
+    return { message: 'login' };
+  }
+}
