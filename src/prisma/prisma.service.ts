8b19f477f7d7040039a2d9daad7cafac5902b18c
QuanggDat <datqit@gmail.com>
2026-08-20 09:35:12 +0700

    feat: enhance PrismaService to use ConfigService for database connection string and add new npm scripts for database management

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../generated/prisma/client';
 
 @Injectable()
 //service này dùng để kết nối DB
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(configService: ConfigService) {
     super({
+      //không hardcode connection string, lấy từ .env qua ConfigService
+      //url: 'postgresql://postgres:Abc123456789@localhost:5434/testdb'
       adapter: new PrismaPg({
-        connectionString: process.env.DATABASE_URL,
+        connectionString: configService.get<string>('DATABASE_URL'),
       }),
     });
+    console.log(
+      'configService DATABASE_URL : ' +
+        configService.get<string>('DATABASE_URL'),
+    );
   }
 }
